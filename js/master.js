d136eac1112eb92433f5ce9b728ce9c775f5cca7
NickGatti <nick.gatti@gmail.com>
2017-12-11 13:25:10 -0700

    sorting orders

--- js/master.js
+++ js/master.js
@@ -1,22 +1,4 @@
 $( function () {
-
-    var baseUrl = 'https://api.gdax.com//products/ETH-USD/trades';
-
-    var xhr = new XMLHttpRequest()
-
-    xhr.addEventListener( 'load', responseRecieved )
-    xhr.open( 'GET', baseUrl )
-    xhr.send()
-
-    function responseRecieved() {
-        if ( this.status < 200 && this.status >= 400 && this.readyState !== 1 ) {
-            console.log( 'Error in API request: ' + this );
-            return
-        }
-        console.log( JSON.parse( this.responseText ) )
-        return JSON.parse( this.responseText )
-    }
-
     let pullBuysOut = ( passedOrderArray ) => {
         return passedOrderArray.filter( ( ele ) => {
             return ele.side === "buy"
@@ -39,5 +21,32 @@ $( function () {
         return passedOrderArray.sort( ( a, b ) => {
             return b.price - a.price
         } )
+    }
+
+    var baseUrl = 'https://api.gdax.com//products/ETH-USD/trades';
+
+    var xhr = new XMLHttpRequest()
+
+    xhr.addEventListener( 'load', responseRecieved )
+    xhr.open( 'GET', baseUrl )
+    xhr.send()
+
+    function responseRecieved() {
+        if ( this.status < 200 && this.status >= 400 && this.readyState !== 1 ) {
+            console.log( 'Error in API request: ' + this );
+            return
+        }
+        run( JSON.parse( this.responseText ) )
+    }
+
+    function run( orders ) {
+        let buyOrders = pullBuysOut( orders )
+        sortHighestToLowest( buyOrders )
+
+        let sellOrders = pullSellsOut( orders )
+        sortLowestToHighest( sellOrders )
+
+        console.log( buyOrders );
+        console.log( sellOrders );
     }
 } );
