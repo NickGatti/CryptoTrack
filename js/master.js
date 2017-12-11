2d79299581a35d2b4eeb427c602ee2e58aab48b7
NickGatti <nick.gatti@gmail.com>
2017-12-11 13:11:59 -0700

    added some functions for sorting orders

--- js/master.js
+++ js/master.js
@@ -1,3 +1,43 @@
 $( function () {
-    console.log( "ready!" );
+
+    var baseUrl = 'https://api.gdax.com//products/ETH-USD/trades';
+
+    var xhr = new XMLHttpRequest()
+
+    xhr.addEventListener( 'load', responseRecieved )
+    xhr.open( 'GET', baseUrl )
+    xhr.send()
+
+    function responseRecieved() {
+        if ( this.status < 200 && this.status >= 400 && this.readyState !== 1 ) {
+            console.log( 'Error in API request: ' + this );
+            return
+        }
+        console.log( JSON.parse( this.responseText ) )
+        return JSON.parse( this.responseText )
+    }
+
+    let pullBuysOut = ( passedOrderArray ) => {
+        return passedOrderArray.filter( ( ele ) => {
+            return ele.side === "buy"
+        } )
+    }
+
+    let pullSellsOut = ( passedOrderArray ) => {
+        return passedOrderArray.filter( ( ele ) => {
+            return ele.side === 'sell'
+        } )
+    }
+
+    let sortLowestToHighest = ( passedOrderArray ) => {
+        return passedOrderArray.sort( ( a, b ) => {
+            return a.price - b.price
+        } )
+    }
+
+    let sortHighestToLowest = ( passedOrderArray ) => {
+        return passedOrderArray.sort( ( a, b ) => {
+            return b.price - a.price
+        } )
+    }
 } );
